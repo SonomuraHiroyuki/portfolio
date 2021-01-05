3cf3a08b341b1af4753a3e06cc28d1ade424afcd
SonomuraHiroyuki <abc332554331@gmail.com>
2021-01-05 14:54:24 +0900

    チャット機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
-   root  'rooms#show'
+   mount ActionCable.server => '/cable'
    devise_for :users
+   root  'rooms#show'
+   resources :messages, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
